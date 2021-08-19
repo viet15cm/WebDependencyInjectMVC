b2641de08a4bd146d25ee5a7459b500349af91b7
viet15cm <viet99cm@gmail.com>
2021-08-19 17:01:07 +0700

    firtcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebDependencyInjectMVC.Models;
+using WebDependencyInjectMVC.Services;
+
+namespace WebDependencyInjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ProductService _productService;
+        public HomeController(ILogger<HomeController> logger , ProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public IActionResult ProductInfo(string productId)
+        {
+            Console.WriteLine(_productService.GetHashCode());
+           var product =  _productService.FindProduct(productId);
+           
+            return View(product);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDependencyInjectMVC.Services
+{
+
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+    }
+    public class ProductService
+    {
+
+        List<Product> products = new List<Product>();
+
+        public ProductService()
+        {
+            Console.WriteLine("Da dang ky dich vu /n");
+            products.AddRange(new Product[] {
+
+                new Product(){ Id ="1" , Name ="Dien Thoai IP" , Price = 50000 },
+                new Product(){ Id ="2" , Name ="Dien Thoai ss" , Price = 60000 },
+                new Product(){ Id ="3" , Name ="Dien Thoai tt" , Price = 70000 },
+                new Product(){ Id ="4" , Name ="Dien Thoai ll" , Price = 80000 },
+            });
+        }
+         
+        public Product FindProduct(string productId)
+        {
+            var qr = from p in products where p.Id == productId select p;
+            return qr.FirstOrDefault();
+        }
+    }
+}
