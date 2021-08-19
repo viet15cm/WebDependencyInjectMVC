d54563f0a040bd374e8fe5734c55c8884c6486cd
viet15cm <viet99cm@gmail.com>
2021-08-19 21:41:01 +0700

    firstcomit1

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace WebDependencyInjectMVC.Services
 {
@@ -18,24 +21,45 @@ namespace WebDependencyInjectMVC.Services
     public class ProductService
     {
 
-        List<Product> products = new List<Product>();
-
-        public ProductService()
+        private readonly List<Product> _products;
+        public ProductService(List<Product> products)
         {
             Console.WriteLine("Da dang ky dich vu /n");
-            products.AddRange(new Product[] {
+
+            _products = products;
+
+            /*products.AddRange(new Product[] {
 
                 new Product(){ Id ="1" , Name ="Dien Thoai IP" , Price = 50000 },
                 new Product(){ Id ="2" , Name ="Dien Thoai ss" , Price = 60000 },
                 new Product(){ Id ="3" , Name ="Dien Thoai tt" , Price = 70000 },
                 new Product(){ Id ="4" , Name ="Dien Thoai ll" , Price = 80000 },
             });
+            */
         }
          
         public Product FindProduct(string productId)
         {
-            var qr = from p in products where p.Id == productId select p;
+            var qr = from p in _products where p.Id == productId select p;
             return qr.FirstOrDefault();
+        }
+
+        public static List<Product> GetListFile()
+        {
+            ConfigurationRoot configurationRoot = RedFile("FileJson/Data.json");
+            var products = configurationRoot.GetSection("Product").Get<List<Product>>();
+            return products;
+        }
+
+        public static ConfigurationRoot RedFile(string path)
+        {
+            ConfigurationRoot configurationRoot;
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile(path);
+            configurationRoot = (ConfigurationRoot)configurationBuilder.Build();
+
+            return configurationRoot;
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,20 @@ namespace WebDependencyInjectMVC
             // DIch vu nay co the inject vao cac dich vu khac , hoac inject vao cac controller
             // Vi du ta inject vao home controler
             //services.AddSingleton<ProductService>();
-            services.AddSingleton<ProductService>();
+            services.AddSingleton<ProductService>((provider) => {
+               
+                var products = new List<Product>();
+                products.AddRange(new Product[] {
+
+                    new Product(){ Id ="1" , Name ="Dien Thoai IP" , Price = 50000 },
+                    new Product(){ Id ="2" , Name ="Dien Thoai ss" , Price = 60000 },
+                    new Product(){ Id ="3" , Name ="Dien Thoai tt" , Price = 70000 },
+                    new Product(){ Id ="4" , Name ="Dien Thoai ll" , Price = 80000 },
+                });
+                var productSevice = new ProductService(products);
+ //               var product = new ProductService(ProductService.GetListFile());
+                return productSevice;
+            });
             services.AddControllersWithViews();
 
         }
